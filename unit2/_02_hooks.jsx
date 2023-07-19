6bea452ea16c936aad87b727608b09cbb8bb4004
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-19 05:50:39 +0530

    hooks

---
+++ unit2/_02_hooks.jsx
@@ -0,0 +1,7 @@
+//React hooks
+
+//These are functions that provide additional functionality to functional components in React. Hooks allow you to reuse stateful logic between components, making it easier to manage state, side effects, and other React features in a more concise and intuitive way.
+
+//ed -> useState, useEffect, useContext, useReducer, useCallback, useRef
+
+//These hooks, along with some others allow you to extend and customize the behaviour of functional components in a more modular and reusable manner. By leveraging hooks, you can write cleaner and more maintainable code while taking full advantage  of React's capabilites.
